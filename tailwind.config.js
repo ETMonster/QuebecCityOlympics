91bc092e707c0b19d7ea5cdb68fe13a887c69cf1
Emmett Tackaberry <emmett.tackaberry@gmail.com>
2023-01-12 21:37:46 -0500

    Finished homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ module.exports = {
     extend: {
       colors: {
         'main': '#1c2e39',
+        'logo-blue': '#0081c8',
+        'logo-yellow': '#fcb131',
+        'logo-red': '#ee334e',
+        'logo-green': '#00a651',
       },
     },
   },
